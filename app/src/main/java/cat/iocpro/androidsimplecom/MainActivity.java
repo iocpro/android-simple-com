b8c116aff3a209168b51fe903377e89c7bb4f086
Enric Mieza <emieza@xtec.cat>
2024-05-07 00:07:09 +0200

    first commit

---
+++ app/src/main/java/cat/iocpro/androidsimplecom/MainActivity.java
@@ -0,0 +1,95 @@
+package cat.iocpro.androidsimplecom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+
+        b.setOnClickListener((View v) -> {
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    // Tasques en background (xarxa)
+                    String data = getDataFromUrl("https://api.myip.com");
+
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            // Tasques a la interfície gràfica (GUI)
+                            TextView tv = findViewById(R.id.textView);
+                            tv.setText(data);
+                        }
+                    });
+                }
+            });
+        });
+    }
+
+    String error = ""; // string field
+    private String getDataFromUrl(String demoIdUrl) {
+
+        String result = null;
+        int resCode;
+        InputStream in;
+        try {
+            URL url = new URL(demoIdUrl);
+            URLConnection urlConn = url.openConnection();
+
+            HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
+            httpsConn.setAllowUserInteraction(false);
+            httpsConn.setInstanceFollowRedirects(true);
+            httpsConn.setRequestMethod("GET");
+            httpsConn.connect();
+            resCode = httpsConn.getResponseCode();
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpsConn.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        in, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                in.close();
+                result = sb.toString();
+            } else {
+                error += resCode;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+}
